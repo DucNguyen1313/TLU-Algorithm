dd6e4e4fd4be5cfc885bb5dd380497d2ebe1052a
DucNguyen1313 <94228599+DucNguyen1313@users.noreply.github.com>
2021-11-16 13:12:37 +0700

    Update 69_Sqrt.cpp

--- Leetcode/Easy/Code/69_Sqrt.cpp
+++ Leetcode/Easy/Code/69_Sqrt.cpp
@@ -5,19 +5,19 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-		int sqrt_v=1;
-		long long s;
-		if (x==0) return 0;
+	int sqrt_v=1;
+	long long s;
+	if (x==0) return 0;
         else{
-			for (int i=0;i<x;i++){
-				s=(long long)i*i;
-				if (s>x){
-					sqrt_v=i-1;
-					break;
-				}
+		for (int i=0;i<x;i++){
+			s=(long long)i*i;
+			if (s>x){
+				sqrt_v=i-1;
+				break;
 			}
-		return sqrt_v;
 		}
+		return sqrt_v;
+	}
     }   
 };
 
